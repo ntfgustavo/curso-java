a8b696d628b2d9fff9f1cd61c5d74365e0c30435
Gustavo Neres <ntfgustavo@gmail.com>
2022-01-31 21:28:16 -0300

    Aula 78 Desafio FOR

--- exercicios/src/controle/DesafioFor.java
+++ exercicios/src/controle/DesafioFor.java
@@ -4,18 +4,18 @@ public class DesafioFor {
 
 	public static void main(String[] args) {
 		
-//		String valor = "#";
+		String valor = "#";
 		
-//		for (int i = 1; i <= 5; i++) {
-//			System.out.println(valor);
-//			valor += "#";
-//		}
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(valor);
+			valor += "#";
+		}
 		
 		// Versão do desafio
 		// Não pode usar valor numérico para controlar o laço!
 		
-		for (String valor = "#"; valor.length() <= 5; valor += "#") {
-			System.out.println(valor);		
+		for (String v = "#"; !v.equals("######"); v += "#") {
+			System.out.println(v);		
 		}
 	}
 }
