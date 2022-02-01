7ce47193ba08b67c28b8d90ea6571009be0d95b9
Gustavo Neres <ntfgustavo@gmail.com>
2022-01-31 22:25:46 -0300

    Aula 82 CONTINUE #01

---
+++ exercicios/src/controle/Continue.java
@@ -0,0 +1,24 @@
+package controle;
+
+import java.util.Iterator;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++) {
+			
+			if (i % 2 == 1) {
+				continue;
+			}
+			
+			System.out.println(i);
+		}
+		
+		for (int i = 0; i <= 10; i++) {			
+			if (i % 2 == 1) continue;			
+			System.out.println(i);
+		}
+		
+	}
+}
