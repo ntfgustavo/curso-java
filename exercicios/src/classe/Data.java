8e74dc0f082e1082548aa785605c20c41c51b851
Gustavo Neres <ntfgustavo@gmail.com>
2022-02-02 13:35:44 -0300

    Aula 101 Desafio Construtor

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -5,7 +5,22 @@ public class Data {
 	int dia;
 	int mes;
 	int ano;
+
+	Data() {
+		
+		dia = 1;
+		mes = 1;
+		ano = 1970;
+		
+	}
 	
+	Data(int diaInicial, int mesInicial, int anoInicial) {
+	
+		dia = diaInicial;
+		mes = mesInicial;
+		ano = anoInicial;	
+		
+	}
 
 	String obterDataFormatada() {
 		
--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -5,15 +5,15 @@ public class DataTeste {
 	public static void main(String[] args) {
 		
 		Data data1 = new Data();
-		data1.dia = 6;
-		data1.mes = 9;
-		data1.ano = 1986;
+//		data1.dia = 6;
+//		data1.mes = 9;
+//		data1.ano = 1986;
 		
 		
-		Data data2 = new Data();		
-		data2.dia = 7;
-		data2.mes = 1;
-		data2.ano = 1984;
+		Data data2 = new Data(7, 1, 1984);		
+//		data2.dia = 7;
+//		data2.mes = 1;
+//		data2.ano = 1984;
 		
 		String dataFormatada1 = data1.obterDataFormatada(); 
 		
