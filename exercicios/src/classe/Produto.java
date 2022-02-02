544a3f703fb8f3ebfc8dbdde5d3d421521b6e9db
Gustavo Neres <ntfgustavo@gmail.com>
2022-02-02 13:29:03 -0300

    Aula 100 Construtor #02

--- exercicios/src/classe/Produto.java
+++ exercicios/src/classe/Produto.java
@@ -5,19 +5,29 @@ public class Produto {
 	String nome;
 	double preco;
 	double desconto;
+
+	Produto() {
+		
+	}
 	
-	
+	Produto(String nomeInicial, double precoInicial, double descontoInicial) {
+
+		nome = nomeInicial;
+		preco = precoInicial;
+		desconto = descontoInicial;
+
+	}
+
 	double precoComDesconto() {
 
 		return preco * (1 - desconto);
-		
+
 	}
 
 	double precoComDesconto(double descontoDoGerente) {
-		
-		return preco * (1 - desconto + descontoDoGerente);
-		
-	}	
 
+		return preco * (1 - desconto + descontoDoGerente);
+
+	}
 
 }
--- exercicios/src/classe/ProdutoTeste.java
+++ exercicios/src/classe/ProdutoTeste.java
@@ -4,10 +4,7 @@ public class ProdutoTeste {
 
 	public static void main(String[] args) {
 		
-		Produto p1 = new Produto();
-		p1.nome = "Notebook";
-		p1.preco = 4356.89;
-		p1.desconto = 0.25;
+		Produto p1 = new Produto("Notebook", 4356.89, 0.25);
 		
 		var p2 = new Produto();
 		p2.nome = "Caneta Preta";
