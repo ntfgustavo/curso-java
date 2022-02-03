7a70654fefb58d907e54c0516170963b91a635ae
Gustavo Neres <ntfgustavo@gmail.com>
2022-02-03 15:49:20 -0300

    Aula 109 Desafio Primeiro Trauma

---
+++ exercicios/src/classe/PrimeiroTrauma.java
@@ -0,0 +1,14 @@
+package classe;
+
+public class PrimeiroTrauma {
+
+	int a = 3; // Não pode mexer aqui!
+	
+	public static void main(String[] args) {
+		
+		// Pode mexer aqui
+		System.out.println(new PrimeiroTrauma().a);
+		// Pode mexer aqui
+		
+	}
+}
