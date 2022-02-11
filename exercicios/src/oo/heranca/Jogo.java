246d9c93a750a62ca0642008737fc80bbdcf51ba
Gustavo Neres <ntfgustavo@gmail.com>
2022-02-10 23:16:31 -0300

    Aula 153 Herança #02

--- exercicios/src/oo/heranca/Jogo.java
+++ exercicios/src/oo/heranca/Jogo.java
@@ -4,22 +4,29 @@ public class Jogo {
 
 	public static void main(String[] args) {
 		
-		Monstro j1 = new Monstro();
-		j1.x = 10;
-		j1.y = 10;
+		Monstro monstro = new Monstro();
+		monstro.x = 10;
+		monstro.y = 10;
 		
-		Heroi j2 = new Heroi();
-		j2.x = 10;
-		j2.y = 11;
+		Heroi heroi = new Heroi();
+		heroi.x = 10;
+		heroi.y = 11;
 		
-		System.out.println(j1.vida);
-		System.out.println(j2.vida);
+		System.out.println("Monstro tem => " + monstro.vida);
+		System.out.println("Heroi tem => " + heroi.vida);
 		
-		j1.atacar(j2);
-		j2.atacar(j1);
+		monstro.atacar(heroi);
+		heroi.atacar(monstro);
 		
-		System.out.println(j1.vida);
-		System.out.println(j2.vida);
+		monstro.atacar(heroi);
+		heroi.atacar(monstro);
+		
+		monstro.andar(Direcao.NORTE);
+		monstro.atacar(heroi);
+		heroi.atacar(monstro);
+		
+		System.out.println("Monstro tem => " + monstro.vida);
+		System.out.println("Heroi tem => " + heroi.vida);
 		
 	}
 }
