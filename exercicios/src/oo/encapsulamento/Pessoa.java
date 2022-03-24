3679a2fe41cbce69e0322b4c6153f6d39c8c3629
Gustavo Neres <ntfgustavo@gmail.com>
2022-03-24 16:56:36 -0300

    Aula 163 Getters/Setters

---
+++ exercicios/src/oo/encapsulamento/Pessoa.java
@@ -0,0 +1,53 @@
+package oo.encapsulamento;
+
+public class Pessoa {
+
+	private String nome;
+	private String sobrenome;
+	private int idade;
+	
+	public Pessoa(String nome, String sobrenome, int idade) {
+		this.setNome(nome);
+		this.setSobrenome(sobrenome);
+		this.setIdade(idade);
+	}
+		
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getNomeCompleto() {
+		return getNome() + " " + getSobrenome();
+	}
+	
+	// Getter
+	public int getIdade() {
+		return idade;
+	}
+	
+	// Setter
+	public void setIdade(int novaIdade) {
+		novaIdade = Math.abs(novaIdade);
+		if(novaIdade >= 0 && novaIdade <= 120) {
+			this.idade = novaIdade;
+		}
+	}
+	
+	@Override
+	public String toString() {	
+		return "Olá eu sou o " + getNome() + " e tenho " + getIdade() + " anos.";
+	}
+	
+}
