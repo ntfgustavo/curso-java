fc38aa6bab7af82268005d87a109770d2cfee59e
Gustavo Neres <ntfgustavo@gmail.com>
2022-03-29 17:45:35 -0300

    Aula 179 Predicate #01

---
+++ exercicios/src/lambdas/Produto.java
@@ -0,0 +1,16 @@
+package lambdas;
+
+public class Produto extends Object {
+
+	final String nome;
+	final double preco;
+	final double desconto;
+	
+	public Produto(String nome, double preco, double desconto) {
+		this.nome = nome;
+		this.preco = preco;
+		this.desconto = desconto;
+	}
+	
+	
+}
