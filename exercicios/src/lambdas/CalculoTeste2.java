f8aea07a6a146313af249018bad9b6004db93555
Gustavo Neres <ntfgustavo@gmail.com>
2022-03-29 02:23:34 -0300

    Aula 175 Usando Interfaces Funcionais

--- exercicios/src/lambdas/CalculoTeste2.java
+++ exercicios/src/lambdas/CalculoTeste2.java
@@ -1,16 +1,25 @@
 package lambdas;
 
+import java.util.function.BinaryOperator;
+
 public class CalculoTeste2 {
 
 	public static void main(String[] args) {
 		
-		Calculo calc = (x, y) -> { return x + y; };		
-		System.out.println(calc.executar(2, 3));
+		// NÃO: int -> Double
+		// double -> Double
+		
+		BinaryOperator<Double> calc = (x, y) -> { return x + y; };		
+		System.out.println(calc.apply(2.0, 3.0));
 		
 		calc = (x, y) -> x * y;
-		System.out.println(calc.executar(2, 3));
+		System.out.println(calc.apply(2.0, 3.0));
 	
-		System.out.println(calc.legal());
-		System.out.println(Calculo.muitoLegal());
+		BinaryOperator<Integer> calc2 = (x, y) -> { return x + y; };		
+		System.out.println(calc2.apply(2, 3));
+		
+		calc2 = (x, y) -> x * y;
+		System.out.println(calc2.apply(2, 3));
+		
 	}
 }
