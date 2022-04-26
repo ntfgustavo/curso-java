7615257892d8571a8ec53642f71ea85cda2023e9
Gustavo Neres <ntfgustavo@gmail.com>
2022-04-25 22:25:23 -0300

    Aula 185 Binary Operator e BiFunction

---
+++ exercicios/src/lambdas/OperadorBinario.java
@@ -0,0 +1,29 @@
+package lambdas;
+
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
+public class OperadorBinario {
+
+	public static void main(String[] args) {
+		
+//		BinaryOperator<Double> media = (Double n1, Double n2) -> (n1 + n2) / 2;
+		BinaryOperator<Double> media = (n1, n2) -> (n1 + n2) / 2;
+		
+		System.out.println(media.apply(9.8, 5.7));
+		
+//		BiFunction<Double, Double, String> resultado = (n1, n2) -> ((n1 + n2) / 2) >= 7 ? "Aprovado" : "Reprovado";
+		
+		BiFunction<Double, Double, String> resultado = (n1, n2) -> {
+			double notaFinal = (n1 + n2) / 2;
+			return notaFinal >= 7 ? "Aprovado" : "Reprovado";
+		};
+		
+		System.out.println(resultado.apply(9.7, 4.1));
+		
+		Function<Double, String> conceito = m -> m >= 7 ? "Aprovado" : "Reprovado";
+		
+		System.out.println(media.andThen(conceito).apply(9.7, 5.1));
+	}
+}
