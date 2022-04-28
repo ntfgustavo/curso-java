8319556849be5357f9838bac15b52ab385a62007
Gustavo Neres <ntfgustavo@gmail.com>
2022-04-28 16:40:37 -0300

    Aula 192 List/Iterator/Stream

---
+++ exercicios/src/streams/ImprimindoObjetos.java
@@ -0,0 +1,34 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ImprimindoObjetos {
+
+	public static void main(String[] args) {
+		
+		List<String> aprovados = Arrays.asList("Lu", "Gui", "Luca", "Ana");
+		
+//		for (int i = 0; i < aprovados.size(); i++) {
+//			System.out.println(aprovados.get(i));
+//		}
+		
+		System.out.println("Usando o foreach...");
+		for (String nome : aprovados) {
+			System.out.println(nome);
+		}
+		
+		System.out.println("\nUsando Iterator...");
+		Iterator<String> iterator = aprovados.iterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+		
+		System.out.println("\nUsando Stream...");
+		Stream<String> stream = aprovados.stream();
+		stream.forEach(System.out::println); // Laço interno!!!
+		
+	}
+}
