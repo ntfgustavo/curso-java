4ab3c774dcd07f7a51f8ebea3a9075ec657197df
Gustavo Neres Teixeira Filho <ntfgustavo@gmail.com>
2022-05-12 23:21:54 -0300

    Aula 198 Desafio Map - Resposta

--- exercicios/src/streams/DesafioMap.java
+++ exercicios/src/streams/DesafioMap.java
@@ -2,7 +2,6 @@ package streams;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
@@ -16,18 +15,23 @@ public class DesafioMap {
 		 * 1. Número para string binária... 6 => "110"
 		 * 2. Inverter a string... "110" => "011"
 		 * 3. Converter de volta para inteiro... "011" => 3
-		 * 
-		 * Integer.
+
 		 */
 		
-		Consumer<Integer> print = System.out::println;
+		UnaryOperator<String> inverter = s -> new StringBuilder(s).reverse().toString();
+		Function<String, Integer> binarioaParaInt = s -> Integer.parseInt(s, 2);
 		
-		Function<Integer, String> strBinaria = numero -> Integer.toBinaryString(numero);
-		UnaryOperator<String> inverterStr = str -> new StringBuilder(str).reverse().toString();
-		Function<String, Integer> converterParaInteiro = str -> Integer.valueOf(str);
-
-		nums.stream().map(strBinaria).map(inverterStr).map(converterParaInteiro).forEach(print);
+		nums.stream().map(Integer::toBinaryString).map(inverter).map(binarioaParaInt).forEach(System.out::println);
 		
+		
+//		Consumer<Integer> print = System.out::println;
+//		
+//		Function<Integer, String> strBinaria = numero -> Integer.toBinaryString(numero);
+//		UnaryOperator<String> inverterStr = str -> new StringBuilder(str).reverse().toString();
+//		Function<String, Integer> converterParaInteiro = str -> Integer.valueOf(str); //Errei, deveria ter usado o parseInt
+//
+//		nums.stream().map(strBinaria).map(inverterStr).map(converterParaInteiro).forEach(print);
+//		
 	}
 	
 }
