c7b46dec440b9223e08d7733cc5036025a2caf95
Gustavo Neres Teixeira Filho <ntfgustavo@gmail.com>
2022-05-29 01:40:30 -0300

    Aula 206 Reduce #03

--- exercicios/src/streams/Aluno.java
+++ exercicios/src/streams/Aluno.java
@@ -6,6 +6,10 @@ public class Aluno {
 	final double nota;
 	final boolean bomComportamento;
 	
+	public Aluno(String nome, double nota) {
+		this(nome, nota, true);
+	}
+	
 	public Aluno(String nome, double nota, boolean bomComportamento) {	
 		this.nome = nome;
 		this.nota = nota;
