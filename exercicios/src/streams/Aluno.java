d07ddf2f2b95feefee9bca122231ee898fd8cc3f
Gustavo Neres Teixeira Filho <ntfgustavo@gmail.com>
2022-05-29 03:05:12 -0300

    Aula 210 Outros Métodos

--- exercicios/src/streams/Aluno.java
+++ exercicios/src/streams/Aluno.java
@@ -20,10 +20,38 @@ public class Aluno {
 	public String toString() {	
 		return nome + " tem nota " + nota;
 	}
-	
-//	@Override
-//	public int hashCode() {	
-//		return 1;
-//	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (bomComportamento ? 1231 : 1237);
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(nota);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (bomComportamento != other.bomComportamento)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (Double.doubleToLongBits(nota) != Double.doubleToLongBits(other.nota))
+			return false;
+		return true;
+	}
 	
 }
