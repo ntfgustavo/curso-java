924aec8a021ce68b7d594e1bb95b194c18bbe4b1
Gustavo Neres Teixeira Filho <ntfgustavo@gmail.com>
2022-06-08 17:05:49 -0300

    Aula 229 Classe Campo: Atributos

--- campo-minado/src/br/com/ntfgustavo/cm/modelo/Campo.java
+++ campo-minado/src/br/com/ntfgustavo/cm/modelo/Campo.java
@@ -1,5 +1,26 @@
 package br.com.ntfgustavo.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Campo {
 
+	private final int linha;
+	private final int coluna;
+	
+	private boolean aberto = false;
+	private boolean minado = false;
+	private boolean marcado = false;
+	
+	private List<Campo> vizinhos = new ArrayList<>();
+	
+	Campo(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+	
+	boolean adicionarVizinho(Campo vizinho) {
+		
+	}
+	
 }
