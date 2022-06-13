6d3c6a0b9554520f3203946f23f1176c4127f538
Gustavo Neres Teixeira Filho <ntfgustavo@gmail.com>
2022-06-13 20:10:34 -0300

    Aula 235 Classe Tabuleiro #01

--- campo-minado/src/br/com/ntfgustavo/cm/modelo/Tabuleiro.java
+++ campo-minado/src/br/com/ntfgustavo/cm/modelo/Tabuleiro.java
@@ -1,5 +1,47 @@
 package br.com.ntfgustavo.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
 
+	private int linhas;
+	private int colunas;
+	private int minas;
+	
+	private final List<Campo> campos = new ArrayList<>();
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+		
+		gerarCampos();
+		associarVizinhos();
+		sortearMinas();
+		
+	}
+
+	private void gerarCampos() {
+		for (int linha = 0; linha < linhas.length; linha++) {
+			for (int coluna = 0; coluna < colunas.length; coluna++) {
+				campos.add(new Campo(linha, coluna));
+			}			
+		}
+	}
+
+	private void associarVizinhos() {
+		for(Campo c1: campos) {
+			for(Campo c2: campos) {
+				c1.adicionarVizinho(c2);
+			}
+		}
+	}
+
+	private void sortearMinas() {
+
+	}
+	
+	
+	
 }
