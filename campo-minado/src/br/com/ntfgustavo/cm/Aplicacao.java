c08c7133aa7815a843cac0478dd9bab737edc1b9
Gustavo Neres Teixeira Filho <ntfgustavo@gmail.com>
2022-06-17 12:44:48 -0300

    Aula 238 Classe Tabuleiro Console

--- campo-minado/src/br/com/ntfgustavo/cm/Aplicacao.java
+++ campo-minado/src/br/com/ntfgustavo/cm/Aplicacao.java
@@ -1,19 +1,14 @@
 package br.com.ntfgustavo.cm;
 
 import br.com.ntfgustavo.cm.modelo.Tabuleiro;
+import br.com.ntfgustavo.cm.visao.TabuleiroConsole;
 
 public class Aplicacao {
 
 	public static void main(String[] args) {
 				
-		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
-		
-		tabuleiro.abrir(3, 3);
-		tabuleiro.alternarMarcacao(4, 4);
-		tabuleiro.alternarMarcacao(4, 5);
-		
-		
-		System.out.println(tabuleiro);
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);		
+		new TabuleiroConsole(tabuleiro);
 	}
 	
 }
