3f023363475f6d675c1b9fa64c897255d14d3efd
Gustavo Neres Teixeira Filho <ntfgustavo@gmail.com>
2022-06-17 13:55:26 -0300

    Aula 240 Correção na Distribuição das Minas

--- campo-minado/src/br/com/ntfgustavo/cm/modelo/Tabuleiro.java
+++ campo-minado/src/br/com/ntfgustavo/cm/modelo/Tabuleiro.java
@@ -59,9 +59,9 @@ public class Tabuleiro {
 		Predicate<Campo> minado = c -> c.isMinado();
 		
 		do {
-			minasArmadas = campos.stream().filter(minado).count();
 			int aleatorio = (int) (Math.random() * campos.size());
 			campos.get(aleatorio).minar();
+			minasArmadas = campos.stream().filter(minado).count();
 		} while(minasArmadas < minas);
 	}
 	
--- campo-minado/src/br/com/ntfgustavo/cm/visao/TabuleiroConsole.java
+++ campo-minado/src/br/com/ntfgustavo/cm/visao/TabuleiroConsole.java
@@ -63,6 +63,7 @@ public class TabuleiroConsole {
 				}
 			}
 			
+			System.out.println(tabuleiro);
 			System.out.println("Você ganhou!!!");
 		} catch (ExplosaoException e) {
 			System.out.println(tabuleiro);
