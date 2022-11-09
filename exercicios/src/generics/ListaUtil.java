a3cad0b427919dd46ef281655325090971abda81
Gustavo Neres Teixeira Filho <ntfgustavo@gmail.com>
2022-11-09 18:49:31 -0300

    Aula 248 Usando Generics em Métodos

---
+++ exercicios/src/generics/ListaUtil.java
@@ -0,0 +1,22 @@
+package generics;
+
+import java.util.List;
+
+public class ListaUtil {
+
+	public static Object getUltimo1(List<?> lista) {
+		return lista.get(lista.size() -1);
+	}
+	
+	public static <T> T getUltimo2(List<T> lista) {
+		return lista.get(lista.size() -1);
+	}
+	
+	
+	// É possivel usar mais de um parametro generico
+//	public static <A, B, C> C teste(A paramA, B paramB) {
+//		C teste = null;
+//		return teste;
+//	}
+	
+}
