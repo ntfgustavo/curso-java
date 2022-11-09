a09cbfe9471baf492e71d8c74679dadd80615320
Gustavo Neres Teixeira Filho <ntfgustavo@gmail.com>
2022-11-09 17:56:09 -0300

    Aula 246 Generics e Herança #01

---
+++ exercicios/src/generics/CaixaIntTeste.java
@@ -0,0 +1,15 @@
+package generics;
+
+public class CaixaIntTeste {
+
+	public static void main(String[] args) {
+		
+		CaixaInt caixaA = new CaixaInt();
+		caixaA.guardar(135);
+		
+		Integer coisaA = caixaA.abrir();
+		System.out.println(coisaA);
+		
+	}
+	
+}
