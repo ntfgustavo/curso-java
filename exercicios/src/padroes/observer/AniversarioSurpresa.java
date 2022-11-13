af45bccfeb5b36e07c1c54a4a35acf062ffdc948
Gustavo Neres Teixeira Filho <ntfgustavo@gmail.com>
2022-11-13 18:08:55 -0300

    Aula 255 Padrão Observer #2

---
+++ exercicios/src/padroes/observer/AniversarioSurpresa.java
@@ -0,0 +1,20 @@
+package padroes.observer;
+
+public class AniversarioSurpresa {
+
+	public static void main(String[] args) {
+		
+		Namorada namorada = new Namorada();
+		Porteiro porteiro = new Porteiro();
+		
+		porteiro.registrarObservador(namorada);
+		
+		porteiro.registrarObservador(e -> {
+			System.out.println("Surpresa via lambda!");
+			System.out.println(e.getMomento());			
+		});
+		porteiro.monitorar();
+		
+	}
+	
+}
---
+++ exercicios/src/padroes/observer/ObservadorChegadaAniversariante.java
@@ -0,0 +1,8 @@
+package padroes.observer;
+
+@FunctionalInterface
+public interface ObservadorChegadaAniversariante {
+
+	public void chegou(EventoChegadaAniversariante evento);
+	
+}
