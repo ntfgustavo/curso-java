8cf4835a172a30a88e31b17b15bf3e48b05edab7
Gustavo Neres Teixeira Filho <ntfgustavo@gmail.com>
2023-03-03 17:50:30 -0300

    Aula 260 Interface Gráfica do Campo Minado #01

---
+++ campo-minado-swing/src/br/com/ntfgustavo/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,15 @@
+package br.com.ntfgustavo.cm.visao;
+
+import javax.swing.JPanel;
+
+import br.com.ntfgustavo.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel {
+	
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+		
+	}
+	
+
+}
---
+++ campo-minado-swing/src/br/com/ntfgustavo/cm/visao/TelaPrincipal.java
@@ -0,0 +1,27 @@
+package br.com.ntfgustavo.cm.visao;
+
+import javax.swing.JFrame;
+
+import br.com.ntfgustavo.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class TelaPrincipal extends JFrame {
+
+	public TelaPrincipal() {
+		Tabuleiro tabuleiro = new Tabuleiro(16, 30, 50);		
+		add(new PainelTabuleiro(tabuleiro));
+		
+		setTitle("Campo Minado");
+		setSize(690, 438);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setVisible(true);		
+	}
+	
+	public static void main(String[] args) {
+		
+		new TelaPrincipal();
+		
+	}
+	
+}
