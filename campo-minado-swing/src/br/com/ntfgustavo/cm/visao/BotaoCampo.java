b1417e09899c8f8ab560310c7ad83eac4ba16914
Gustavo Neres Teixeira Filho <ntfgustavo@gmail.com>
2023-03-04 09:54:28 -0300

    Adicionando classe BotãoCampo

---
+++ campo-minado-swing/src/br/com/ntfgustavo/cm/visao/BotaoCampo.java
@@ -0,0 +1,14 @@
+package br.com.ntfgustavo.cm.visao;
+
+import javax.swing.JButton;
+
+import br.com.ntfgustavo.cm.modelo.Campo;
+
+@SuppressWarnings("serial")
+public class BotaoCampo extends JButton {
+
+	public BotaoCampo(Campo campo) {
+		
+	}
+	
+}
