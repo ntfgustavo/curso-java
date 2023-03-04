6fa9be9ff5721254a9dea67655b9024b51b0bb30
Gustavo Neres Teixeira Filho <ntfgustavo@gmail.com>
2023-03-04 11:20:06 -0300

    Aula 265 Interface Gráfica do Campo Minado #05

--- campo-minado-swing/src/br/com/ntfgustavo/cm/visao/BotaoCampo.java
+++ campo-minado-swing/src/br/com/ntfgustavo/cm/visao/BotaoCampo.java
@@ -24,6 +24,7 @@ public class BotaoCampo extends JButton implements CampoObservador, MouseListene
 	public BotaoCampo(Campo campo) {
 		this.campo = campo;
 		setBackground(BG_PADRAO);
+		setOpaque(true);
 		setBorder(BorderFactory.createBevelBorder(0));
 		
 		addMouseListener(this);
@@ -49,25 +50,33 @@ public class BotaoCampo extends JButton implements CampoObservador, MouseListene
 	}
 
 	private void aplicarEstiloPadrao() {
-		// TODO Auto-generated method stub
-		
+		setBackground(BG_PADRAO);
+		setText("");		
 	}
 
 	private void aplicarEstiloExplodir() {
-		// TODO Auto-generated method stub
-		
+		setBackground(BG_EXPLODIR);
+		setForeground(Color.WHITE);
+		setText("X");
 	}
 
 	private void aplicarEstiloMarcar() {
-		// TODO Auto-generated method stub
-		
+		setBackground(BG_MARCAR);
+		setForeground(Color.BLACK);
+		setText("M");		
 	}
 	
 	private void aplicarEstiloAbrir() {
-		setBackground(BG_PADRAO);
+		
 		setBorder(BorderFactory.createLineBorder(Color.GRAY));
 		
+		if(campo.isMinado()) {
+			setBackground(BG_EXPLODIR);
+			return;
+		}
 		
+		setBackground(BG_PADRAO);
+				
 		switch (campo.minasNaVizinhaca()) {
 		case 1:
 			setForeground(TEXTO_VERDE);
