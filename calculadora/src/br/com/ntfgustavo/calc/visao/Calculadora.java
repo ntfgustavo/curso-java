56c25436b92e1ccbd9a3868ae13d6e79b4f404af
Gustavo Neres Teixeira Filho <ntfgustavo@gmail.com>
2023-03-08 15:13:35 -0300

    Aula 267 Tela Principal da Calculadora

---
+++ calculadora/src/br/com/ntfgustavo/calc/visao/Calculadora.java
@@ -0,0 +1,21 @@
+package br.com.ntfgustavo.calc.visao;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Calculadora extends JFrame {
+
+	public Calculadora() {
+	
+		setSize(232, 322);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		
+	}
+	
+	public static void main(String[] args) {
+		new Calculadora();
+	}
+	
+}
