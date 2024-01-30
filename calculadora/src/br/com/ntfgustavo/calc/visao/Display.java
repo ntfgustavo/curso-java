3a37d674a462acb4da086c1d67c96799fdf50a52
Gustavo Neres <ntfgustavo@gmail.com>
2024-01-30 10:41:49 -0300

    Aula 270 Componente Display

--- calculadora/src/br/com/ntfgustavo/calc/visao/Display.java
+++ calculadora/src/br/com/ntfgustavo/calc/visao/Display.java
@@ -1,15 +1,26 @@
 package br.com.ntfgustavo.calc.visao;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Display extends JPanel {
 
+	private final JLabel label;
+	
 	public Display() {
+		setBackground(new Color(46, 49, 50));
+		label = new JLabel("1234,56");
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("courier", Font.PLAIN, 30));
 		
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
 		
+		add(label);
 	}
 	
 }
