1e374738014d9b616dc5418d2c49c267d54760ad
Gustavo Neres <ntfgustavo@gmail.com>
2024-02-07 17:39:41 -0300

    Aula 317 Desafio Consultar Registros - Resposta

--- exercicios/src/jdbc/ConsutarPessoas2.java
+++ exercicios/src/jdbc/ConsutarPessoas2.java
@@ -4,12 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
-import com.mysql.cj.xdevapi.PreparableStatement;
 
 public class ConsutarPessoas2 {
 	
@@ -17,32 +14,34 @@ public class ConsutarPessoas2 {
 	
 		Scanner entrada = new Scanner(System.in);
 
-		System.out.println("Informe o nome da pessoa: ");
-		String nome = entrada.nextLine();
-		
 		Connection conexao = FabricaConexao.getConexao();
 		String sql = "SELECT * FROM pessoas WHERE nome LIKE ? ";
 		
-		PreparedStatement stmt = conexao.prepareStatement(sql);
-		stmt.setString(1, "%" + nome + "%");		
+		System.out.println("Informe o valor pra pesquisa: ");
+		String valor = entrada.nextLine();
 		
+		
+		PreparedStatement stmt = conexao.prepareStatement(sql);
+		stmt.setString(1, "%" + valor + "%");				
 		ResultSet resultado = stmt.executeQuery();
 		
 		List<Pessoa> pessoas = new ArrayList<>();
 		
 		while(resultado.next()) {
 			
-			pessoas.add(new Pessoa(resultado.getInt("codigo"), resultado.getString("nome")));
+			int codigo = resultado.getInt("codigo");
+			String nome = resultado.getString("nome");			
+			pessoas.add(new Pessoa(codigo, nome));
 			
 		}
 		
 		for(Pessoa p: pessoas) {
 			System.out.println(p.getCodigo() + " ==> " + p.getNome());
 		}	
-		
-		entrada.close();
+				
 		stmt.close();
 		conexao.close();
+		entrada.close();
 		
 	}
 	
