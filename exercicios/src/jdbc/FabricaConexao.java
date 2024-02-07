9a25fe7b25df4df72f7e30bc8200b336cbc0c261
Gustavo Neres <ntfgustavo@gmail.com>
2024-02-07 15:18:23 -0300

    Aula 313 Criar Tabela

--- exercicios/src/jdbc/FabricaConexao.java
+++ exercicios/src/jdbc/FabricaConexao.java
@@ -9,7 +9,7 @@ public class FabricaConexao {
 	public static Connection getConexao() {
 		
 		try {
-			final String url = "jdbc:mysql://localhost?verifyServerCerificate=false&userSSL=true";
+			final String url = "jdbc:mysql://localhost/curso_java?verifyServerCerificate=false&userSSL=true";
 			final String usuario = "root";
 			final String senha = "k8n4eGusFer.";
 			
