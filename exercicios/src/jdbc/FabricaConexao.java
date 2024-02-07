b26c7b1a14515079557d06550a1c97e2d92afce9
Gustavo Neres <ntfgustavo@gmail.com>
2024-02-07 14:56:02 -0300

    Aula 312 Fábrica de Conexões

---
+++ exercicios/src/jdbc/FabricaConexao.java
@@ -0,0 +1,24 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FabricaConexao {
+
+	public static Connection getConexao() {
+		
+		try {
+			final String url = "jdbc:mysql://localhost?verifyServerCerificate=false&userSSL=true";
+			final String usuario = "root";
+			final String senha = "k8n4eGusFer.";
+			
+			return DriverManager.getConnection(url, usuario, senha);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+	
+	
+}
