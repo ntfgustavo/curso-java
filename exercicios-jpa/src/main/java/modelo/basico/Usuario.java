6946eba232dd67fc784748cc99b2d1e42d90682b
Gustavo Neres <ntfgustavo@gmail.com>
2024-06-11 15:27:53 -0300

    Aula 335 Novo Usuário

--- exercicios-jpa/src/main/java/modelo/basico/Usuario.java
+++ exercicios-jpa/src/main/java/modelo/basico/Usuario.java
@@ -1,12 +1,15 @@
 package modelo.basico;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Usuario {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String nome;
