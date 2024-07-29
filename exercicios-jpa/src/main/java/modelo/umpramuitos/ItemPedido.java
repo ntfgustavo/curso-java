cde99090a9722b0e06e2d593082545a64b9189b8
Gustavo Neres <ntfgustavo@gmail.com>
2024-07-29 16:54:22 -0300

    Aula 353 Um Pra Muitos #03

--- exercicios-jpa/src/main/java/modelo/umpramuitos/ItemPedido.java
+++ exercicios-jpa/src/main/java/modelo/umpramuitos/ItemPedido.java
@@ -2,6 +2,7 @@ package modelo.umpramuitos;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -19,7 +20,7 @@ public class ItemPedido {
 	@ManyToOne
 	private Pedido pedido;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.EAGER) // Esse é o valor default de fetch (...ToOne -> EAGER)
 	private Produto produto;
 	
 	@Column(nullable = false)
--- exercicios-jpa/src/main/java/modelo/umpramuitos/Pedido.java
+++ exercicios-jpa/src/main/java/modelo/umpramuitos/Pedido.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -20,7 +21,7 @@ public class Pedido {
 	@Column(nullable = false)
 	private Date data;
 	
-	@OneToMany(mappedBy = "pedido")
+	@OneToMany(mappedBy = "pedido", fetch = FetchType.LAZY) // Esse é o valor default de fetch (...ToMany -> LAZY)
 	private List<ItemPedido> itens;
 
 	public Pedido() {
