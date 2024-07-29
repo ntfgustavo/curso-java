23b80bb36d20f711c78e6c14097336b519ee68b9
Gustavo Neres <ntfgustavo@gmail.com>
2024-07-29 16:51:32 -0300

    Aula 352 Um Pra Muitos #02

--- exercicios-jpa/src/main/java/modelo/umpramuitos/Pedido.java
+++ exercicios-jpa/src/main/java/modelo/umpramuitos/Pedido.java
@@ -1,12 +1,14 @@
 package modelo.umpramuitos;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Pedido {
@@ -17,6 +19,9 @@ public class Pedido {
 	
 	@Column(nullable = false)
 	private Date data;
+	
+	@OneToMany(mappedBy = "pedido")
+	private List<ItemPedido> itens;
 
 	public Pedido() {
 		this(new Date());
@@ -42,7 +47,13 @@ public class Pedido {
 	public void setData(Date data) {
 		this.data = data;
 	}
-	
-	
+
+	public List<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(List<ItemPedido> itens) {
+		this.itens = itens;
+	}
 	
 }
