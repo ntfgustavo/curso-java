4d180709cdbc0a898df427f4d14910146446bceb
Gustavo Neres <ntfgustavo@gmail.com>
2024-07-29 15:54:18 -0300

    Aula 351 Um Pra Muitos #01

---
+++ exercicios-jpa/src/main/java/modelo/umpramuitos/Pedido.java
@@ -0,0 +1,48 @@
+package modelo.umpramuitos;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pedido {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private Date data;
+
+	public Pedido() {
+		this(new Date());
+	}
+	
+	public Pedido(Date data) {
+		super();
+		this.data = data;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
+	
+	
+}
