e65fdfe722180cefb51a8ae72e335bd93f533beb
Gustavo Neres <ntfgustavo@gmail.com>
2024-10-08 12:14:13 -0300

    Aula 370 Exports To

--- app_calculo/src/module-info.java
+++ app_calculo/src/module-info.java
@@ -3,4 +3,6 @@ module app.calculo {
 	requires transitive app.logging;
 	exports br.com.cod3r.app.calculo;
 	
+	exports br.com.cod3r.app.calculo.interno to app.financeiro;
+	
 }
--- app_financeiro/src/br/com/cob3r/app/financeiro/Teste.java
+++ app_financeiro/src/br/com/cob3r/app/financeiro/Teste.java
@@ -1,6 +1,7 @@
 package br.com.cob3r.app.financeiro;
 
 import br.com.cod3r.app.calculo.Calculadora;
+import br.com.cod3r.app.calculo.interno.OperacoesAritmeticas;
 
 public class Teste {
 
@@ -8,6 +9,10 @@ public class Teste {
 		
 		Calculadora calc = new Calculadora();
 		System.out.println(calc.soma(2, 3, 4));
+	
+		OperacoesAritmeticas op = new OperacoesAritmeticas();
+		System.out.println(op.soma(4, 5, 6));
+		
 		
 	}
 	
