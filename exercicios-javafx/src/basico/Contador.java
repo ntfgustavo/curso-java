b531cb587c57b20327feaad307a245596e100bb5
Gusavo Neres <ntfgustavo@gmail.com>
2024-11-28 13:39:26 -0300

    Aula 381 Contador #04

--- exercicios-javafx/src/basico/Contador.java
+++ exercicios-javafx/src/basico/Contador.java
@@ -22,12 +22,14 @@ public class Contador extends Application {
 		labelNumero.getStyleClass().add("numero");
 		
 		Button botaoDecremento = new Button("-");
+		botaoDecremento.getStyleClass().add("botoes");
 		botaoDecremento.setOnAction(e -> {
 			contador--;
 			labelNumero.setText(Integer.toString(contador));
 		});
 		
 		Button botaoIncremento = new Button("+");
+		botaoIncremento.getStyleClass().add("botoes");
 		botaoIncremento.setOnAction(e -> {
 			contador++;
 			labelNumero.setText(Integer.toString(contador));
