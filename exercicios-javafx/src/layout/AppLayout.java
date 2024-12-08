5a04d19e34924119ae52d5b83136a938830bad8c
Gusavo Neres <ntfgustavo@gmail.com>
2024-12-07 21:10:13 -0300

    Aula 386 BorderPane

--- exercicios-javafx/src/layout/AppLayout.java
+++ exercicios-javafx/src/layout/AppLayout.java
@@ -1,6 +1,7 @@
 package layout;
 
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -8,8 +9,12 @@ public class AppLayout extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-						
-		Scene principal = new Scene(new TesteAnchorPane(), 800, 600);
+		Parent raiz = null;	
+		
+//		raiz = new TesteAnchorPane();
+		raiz = new TesteBorderPane();
+		
+		Scene principal = new Scene(raiz, 800, 600);
 		
 		primaryStage.setScene(principal);
 		primaryStage.setTitle("Gerenciadores de Layout");
